65d3f14fd01034c7fc6cde409be92a92740dd75d
wppaing <wintphyopaing.p@gmail.com>
2023-03-15 07:53:27 +0630

    url slug implementation wip

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,10 +1,14 @@
 const Product = require('../models/Product');
+const slugify = require('slugify');
 
 exports.createProduct = (req, res, next) => {
-  const {} = req.body;
+  const { name } = req.body;
+  let slug = slugify(name, { lower: true, strict: true });
+  slug = slug + '-' + '63d94a44a8e17a8975e63d19';
 
   res.json({
     message: 'success',
+    data: { ...req.body, slug },
   });
 };
 
